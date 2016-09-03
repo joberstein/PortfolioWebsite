fa7ad92e862dd426c5a734ec51dc926ce1321814
Jesse Oberstein <oberstein.j@husky.neu.edu>
2016-09-03 01:15:30 -0400

    Deprecated original lightbox implementation in favor of one that is more flexible and can show videos. Updated resume and some page styles. Fixed bug where I accidentally removed the tooltip file import.

---
+++ app/static/js/src/initialize-prettyphoto.js
@@ -0,0 +1,7 @@
+$("a[rel^='prettyPhoto']").prettyPhoto({
+    animationSpeed: 'normal', /* fast/slow/normal */
+    opacity: 0.80, /* Value between 0 and 1 */
+    showTitle: true, /* true/false */
+    autoplay: false, /* Automatically start videos: True/False */
+    social_tools: '<div></div>'
+});
